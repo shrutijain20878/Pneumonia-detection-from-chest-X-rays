ff418b6dfd7bde7ff65f2ac8655cd4f3435335c1
Shruti Jain <shruti.jain@rdsolutions.in>
2026-01-03 19:36:45 +0530

    Fix port

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from torchvision import models, transforms
 from PIL import Image
 import io
+import os
 
 # --------------------------------------------------
 # Global settings
@@ -97,4 +98,5 @@ def predict():
 # Run Server
 # --------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
